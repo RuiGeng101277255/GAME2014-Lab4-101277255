bd4da451455f635ee6a92b1e66d76e2133e86891
RuiGeng <ruichen.gengli@georgebrown.ca>
2021-10-06 18:25:56 -0400

    added enemy bullet and script and prefab

---
+++ Assets/[Scripts]/BulletBehaviourScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviourScript : MonoBehaviour
+{
+    [Header("Bullet Movement")]
+    [Range(0.0f, 0.5f)]
+    public float speed;
+    public BoundsStruct Bbounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
+        transform.position -= new Vector3(0.0f, speed, 0.0f);
+    }
+    
+    private void CheckBounds()
+    {
+        if (transform.position.y < Bbounds.Max)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
