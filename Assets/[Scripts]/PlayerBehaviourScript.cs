aa4bf45b38cd06246cff2338157327452a9a5d46
RuiGeng <ruichen.gengli@georgebrown.ca>
2021-10-06 17:50:54 -0400

    initial player movement and decay val (no boundary yet)

--- Assets/[Scripts]/PlayerBehaviourScript.cs
+++ Assets/[Scripts]/PlayerBehaviourScript.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerBehaviourScript : MonoBehaviour
 {
+    [Header("Player Movement")]
+    public float HorizontalForce;
+    [Range(0.0f, 1.0f)]
+    public float VelDecay;
+
+    private Rigidbody2D playerRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
+        var x = Input.GetAxisRaw("Horizontal");
+
+        playerRB.AddForce(new Vector2(x * HorizontalForce, 0.0f));
+
+        playerRB.velocity *= (1.0f - VelDecay);
+    }
+
+    private void CheckBounds()
+    {
+
     }
 }
